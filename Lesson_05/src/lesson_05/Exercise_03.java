8d5fdf24d89b4d2c3823ec8c9754f4dd8fbe59c8
Antonio Bandeira <antonioviniciusvb@outlook.com>
2023-11-13 16:11:45 -0300

    Lesson 5 - Improve Resolution Exercise 04

--- Lesson_05/src/lesson_05/Exercise_03.java
+++ Lesson_05/src/lesson_05/Exercise_03.java
@@ -20,39 +20,21 @@ public class Exercise_03 {
 		System.out.println("-- Elements on Second Diagonal -- ");
 		for (int i : elementsOnSecondDiagonal)
 			System.out.println(i);
-		
+
 		sumMainDiagonal = sumArrayElements(elementsOnMainDiagonal);
 		System.out.printf("Sum Elements on Main Diagonal: %d\n", sumMainDiagonal);
 		sumSecondDiagonal = sumArrayElements(elementsOnSecondDiagonal);
 		System.out.printf("Sum Elements on Second Diagonal: %d\n", sumSecondDiagonal);
-		
 
 	}
 
 	public static int[] findElementsOnMainDiagonal(int[][] matrix) {
 
-		int indexFind = 0, indexMatrix = 0;
 		int[] elementsOnMainDiagonal = new int[3];
 
-		for (int line = 0; line < matrix.length; line++) {
+		for (int index = 0; index < matrix.length; index++) {
 
-			for (int column = 0; column < matrix[line].length; column++) {
-
-				if (column == indexFind) {
-
-					if (indexMatrix + 1 > elementsOnMainDiagonal.length - 1) {
-
-						elementsOnMainDiagonal[indexMatrix] = matrix[line][column];
-						break;
-					}
-
-					indexFind++;
-
-					elementsOnMainDiagonal[indexMatrix++] = matrix[line][column];
-
-					break;
-				}
-			}
+			elementsOnMainDiagonal[index] = matrix[index][index];
 		}
 
 		return elementsOnMainDiagonal;
@@ -60,43 +42,25 @@ public class Exercise_03 {
 
 	public static int[] findElementsOnSecondDiagonal(int[][] matrix) {
 
-		int indexFind = matrix.length - 1, indexMatrix = 0;
 		int[] elementsOnSecondDiagonal = new int[3];
 
-		for (int line = 0; line < matrix.length; line++) {
+		for (int index = 0; index < matrix.length; index++) {
 
-			for (int column = matrix[line].length; column >= 0; column--) {
-
-				if (column == indexFind) {
-
-					if (indexMatrix + 1 > elementsOnSecondDiagonal.length - 1) {
-
-						elementsOnSecondDiagonal[indexMatrix] = matrix[line][column];
-						break;
-					}
-
-					indexFind--;
-
-					elementsOnSecondDiagonal[indexMatrix++] = matrix[line][column];
-
-					break;
-				}
-			}
+			elementsOnSecondDiagonal[index] = matrix[index][matrix.length - 1 - index];
 		}
-
+		
 		return elementsOnSecondDiagonal;
 	}
-	
-	
+
 	private static int sumArrayElements(int[] array) {
-		
+
 		int sum = 0;
-		
+
 		for (var element : array) {
 			sum += element;
 		}
-		
+
 		return sum;
-		
+
 	}
 }
